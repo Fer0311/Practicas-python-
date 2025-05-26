28a371c12abaa55c3da4ec4828274251c5c41be8
Fer0311 <fervm0305@gmail.com>
2025-05-26 11:51:10 -0600

    Add files via upload

---
+++ Practica 5.py
@@ -0,0 +1,10 @@
+Calificacion = 100
+
+if Calificacion >=90:
+    print("Excelente")
+elif Calificacion >=70:
+    print("Aprobado")
+elif Calificacion >=60:
+    print("Regular")
+else:
+    print("Reprobado")            

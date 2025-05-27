ede61fc4f5e3ce2a6b4b20f1918bc29226dd07e1
Fer0311 <fervm0305@gmail.com>
2025-05-27 11:54:47 -0600

    Add files via upload

---
+++ practica6.py
@@ -0,0 +1,7 @@
+numero_secreto=7
+intento = int (input ("ingresa un numero entre 1 y 10"))
+while intento != numero_secreto :
+    print ("valor incorrecto")
+    intento = int (input("escribe otro numero"))
+    print("numero correcto")
+    
